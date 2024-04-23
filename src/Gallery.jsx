ae0c2c43cbbf154e2164fc4f286eeef81e6367e5
Dariusz Okoński <darek200180@gmail.com>
2024-04-23 14:54:13 +0200

    get data at Gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import React from 'react';
 
 const url =
-  'https://api.unsplash.com/search/photos?client_id=Us8iBZMzobgIfEkEirQnscy3-k70l-9lGlJfB7PzexM&query=office';
+  'https://api.unsplash.com/search/photos?client_id=Us8iBZMzobgIfEkEirQnscy3-k70l-9lGlJfB7PzexM&query=cat';
 
 const Gallery = () => {
   const response = useQuery({
@@ -14,12 +14,47 @@ const Gallery = () => {
     },
   });
 
-  console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className='image-container'>
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No results found</h4>
+      </section>
+    );
+  }
 
   return (
-    <div>
-      <h3>Gallery</h3>
-    </div>
+    <section className='image-container'>
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className='img'
+          />
+        );
+      })}
+    </section>
   );
 };
 
