3e413145e56d8635bb86ed8da133c4ddf946f815
Dariusz Okoński <darek200180@gmail.com>
2024-04-23 14:47:54 +0200

    get data at Gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
+const url =
+  'https://api.unsplash.com/search/photos?client_id=Us8iBZMzobgIfEkEirQnscy3-k70l-9lGlJfB7PzexM&query=office';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+
+  console.log(response);
+
   return (
     <div>
       <h3>Gallery</h3>
