6b4f4132dfa5040b7d8f7e56459dae247b4cb5a9
Dariusz Okoński <darek200180@gmail.com>
2024-04-23 13:53:25 +0200

    add context

---
+++ src/SearchForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchForm = () => {
+  return (
+    <div>
+      <h3>SearchForm</h3>
+    </div>
+  );
+};
+
+export default SearchForm;
---
+++ src/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThemeToggle = () => {
+  return (
+    <div>
+      <h3>ThemeToggle</h3>
+    </div>
+  );
+};
+
+export default ThemeToggle;
