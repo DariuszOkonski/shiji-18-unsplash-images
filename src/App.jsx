b80f6c07e5a2026b29aeabf0e253dd9e202a4f71
Dariusz Okoński <darek200180@gmail.com>
2024-04-23 14:43:44 +0200

    add queryProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Gallery from './Gallery';
 import SearchForm from './SearchForm';
 import ThemeToggle from './ThemeToggle';
 
+// const secretKey = 'Us8iBZMzobgIfEkEirQnscy3-k70l-9lGlJfB7PzexM'
+// https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+// GET /search/photos
+// https://api.unsplash.com/search/photos?page=1&query=office
+
 const App = () => {
   return (
     <main>
