282b28c9a0dd198886435ce7006c4ac4c1096367
Dariusz Okoński <darek200180@gmail.com>
2024-04-23 14:17:58 +0200

    add styles for dark-theme

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -8,7 +8,10 @@ const ThemeToggle = () => {
     <section className='toggle-container'>
       <button className='dark-toggle' onClick={toggleDarkTheme}>
         {isDarkTheme ? (
-          <BsFillMoonFill className='toggle-icon' />
+          <BsFillMoonFill
+            className='toggle-icon'
+            style={{ color: '#f0f0f0' }}
+          />
         ) : (
           <BsFillSunFill className='toggle-icon' />
         )}
--- src/contex.jsx
+++ src/contex.jsx
@@ -7,8 +7,10 @@ export const AppProvider = ({ children }) => {
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
-
     setIsDarkTheme(newDarkTheme);
+
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', newDarkTheme);
   };
 
   return (
